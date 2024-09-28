d257bb8064a032a498c153921b638231a618ec9a
Victoria Fakunle <v.fakunle@alustudent.com>
2024-09-28 14:28:39 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
+"""
+This is the file storage class for AirBnB
+
+This class serializes instances to a JSON file and deserializes JSON file to instances.
+"""
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -10,21 +15,26 @@ from models.place import Place
 from models.review import Review
 import shlex
 
-
 class FileStorage:
-    """This class serializes instances to a JSON file and
-    deserializes JSON file to instances
+    """
+    This class serializes instances to a JSON file and deserializes JSON file to instances.
+
     Attributes:
-        __file_path: path to the JSON file
-        __objects: objects will be stored
+        __file_path (str): path to the JSON file
+        __objects (dict): objects will be stored
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
+        """
+        Returns a dictionary of objects.
+
+        Args:
+            cls (class, optional): class of objects to return. Defaults to None.
+
+        Returns:
+            dict: dictionary of objects
         """
         dic = {}
         if cls:
@@ -39,16 +49,19 @@ class FileStorage:
             return self.__objects
 
     def new(self, obj):
-        """sets __object to given obj
+        """
+        Sets __object to given obj.
+
         Args:
-            obj: given object
+            obj (object): given object
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-        """serialize the file path to JSON file path
+        """
+        Serialize the file path to JSON file path.
         """
         my_dict = {}
         for key, value in self.__objects.items():
@@ -57,7 +70,8 @@ class FileStorage:
             json.dump(my_dict, f)
 
     def reload(self):
-        """serialize the file path to JSON file path
+        """
+        Deserialize the file path to JSON file path.
         """
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
@@ -68,13 +82,18 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ delete an existing element
+        """
+        Delete an existing element.
+
+        Args:
+            obj (object, optional): object to delete. Defaults to None.
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
 
     def close(self):
-        """ calls reload()
+        """
+        Calls reload().
         """
         self.reload()
