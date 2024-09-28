25a2259215cf63eb06815175d47a5bca6d9938ce
Victoria Fakunle <v.fakunle@alustudent.com>
2024-09-28 14:31:33 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,6 +15,7 @@ from models.place import Place
 from models.review import Review
 import shlex
 
+
 class FileStorage:
     """
     This class serializes instances to a JSON file and deserializes JSON file to instances.
@@ -23,6 +24,7 @@ class FileStorage:
         __file_path (str): path to the JSON file
         __objects (dict): objects will be stored
     """
+
     __file_path = "file.json"
     __objects = {}
 
@@ -42,11 +44,12 @@ class FileStorage:
             for key in dictionary:
                 partition = key.replace('.', ' ')
                 partition = shlex.split(partition)
-                if (partition[0] == cls.__name__):
+                if partition[0] == cls.__name__:
                     dic[key] = self.__objects[key]
-            return (dic)
+            return dic
         else:
             return self.__objects
+
 
     def new(self, obj):
         """
@@ -59,6 +62,7 @@ class FileStorage:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
+
     def save(self):
         """
         Serialize the file path to JSON file path.
@@ -66,34 +70,4 @@ class FileStorage:
         my_dict = {}
         for key, value in self.__objects.items():
             my_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(my_dict, f)
-
-    def reload(self):
-        """
-        Deserialize the file path to JSON file path.
-        """
-        try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                for key, value in (json.load(f)).items():
-                    value = eval(value["__class__"])(**value)
-                    self.__objects[key] = value
-        except FileNotFoundError:
-            pass
-
-    def delete(self, obj=None):
-        """
-        Delete an existing element.
-
-        Args:
-            obj (object, optional): object to delete. Defaults to None.
-        """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
-
-    def close(self):
-        """
-        Calls reload().
-        """
-        self.reload()
+        with open(self.__file_path, 'w', encoding
